14382367fbbd945d383dbf69bf074447274317a1
= <3lian3.pereira@gmail.com>
2022-10-02 22:18:09 -0300

    função escrever letra correta

--- canvas.js
+++ canvas.js
@@ -1,15 +1,16 @@
+// let largura = 600
 function desenharCanvas(){
   tabuleiro.lineWidth = 8;
   tabuleiro.lineCap = "round";
   tabuleiro.lineJoin = "round";
-  tabuleiro.fillStyle = "#F3F5FC";
+  tabuleiro.fillStyle = "#333138";
   tabuleiro.strokeStyle = "#ff006e";
 
   //manipulação
   tabuleiro.fillRect(0,0,1200,800);
   tabuleiro.beginPath();
-  tabuleiro.moveTo(900,500);
-  tabuleiro.lineTo(650,500);
+  tabuleiro.moveTo(650,500); //900,500
+  tabuleiro.lineTo(900,500); //650,500
   tabuleiro.stroke();
   tabuleiro.closePath();
 
@@ -19,14 +20,24 @@ function desenharLinhas(){
   tabuleiro.lineWidth = 6;
   tabuleiro.lineCap = "round";
   tabuleiro.lineJoin = "round";
-  tabuleiro.fillStyle = "#F3F5FC";
+  tabuleiro.fillStyle = "#333138";
   tabuleiro.strokeStyle = "#ff006e";
 
   let largura = 600/palavraSecreta.length;
   for (let i = 0; i < palavraSecreta.length; i++) {
-    tabuleiro.moveTo(500+(largura*i),640)
+    tabuleiro.moveTo(503+(largura*i),640)
     tabuleiro.lineTo(550+(largura*i),640)    
   }
   tabuleiro.stroke()
   tabuleiro.closePath()
+}
+function escreverLetraCorreta(index){
+  tabuleiro.font = 'bold 52px Inter'
+  tabuleiro.lineWidth = 6
+  tabuleiro.lineCap = "round" 
+  tabuleiro.lineJoin = "round"
+  tabuleiro.fillStyle = "#ff006e"
+  let largura = 600/palavraSecreta.length //tamanho max.600px
+  tabuleiro.fillText(palavraSecreta[index],505+(largura*index),620)
+  tabuleiro.stroke()
 }
--- forca.js
+++ forca.js
@@ -1,23 +1,57 @@
 let palavras = ["ALURA","ORACLE","FORCA","HTML","JAVASCRIPT","LOGICA","PROGRAMACAO"];
 let tabuleiro = document.getElementById('forca').getContext('2d');
 let palavraSecreta = "";
+let letras = []
 
 function escolherPalavraSecreta(){
   let palavra = palavras[Math.floor(Math.random()*palavras.length)]
   palavraSecreta = palavra;
   console.log(palavraSecreta);
   return palavra;
+}
+// Math.floor => arredonda para baixo.
+//Math.random => sorteia palavra
 
+function verificarLetra(key){
+  let estado = false
+  //numeros da tabela ASCII; indexOf => primeira ocorrência de uma string 
+  
+  if (key >= 65 && letras.indexOf(key) || key <= 90 && letras.indexOf(key)) {
+    letras.push(key) //inserindo dados no array letras.
+    console.log(key)
+    console.log(letras)
+    return estado;
+  }
+  else{
+    estado = true
+    letras.push(key)
+    console.log(key)
+    console.log(letras,"if true")
+    return estado;
+  }
 }
 
 function iniciarJogo(){
   document.getElementById('div-desaparece').style.display = "none";
-
   escolherPalavraSecreta();
+
   desenharCanvas();
   desenharLinhas();
+
+  document.onkeydown = (e)=> {
+
+    let letra = e.key.toUpperCase()
+
+    if (verificarLetra(letra) && palavraSecreta.includes(letra)) {
+      //includes => percorre a string e verifica se tem a string desejada
+      for (let i = 0; i < palavraSecreta.length; i++) {
+        if (palavraSecreta[i] === letra){
+          escreverLetraCorreta(i)
+        }
+               
+      }
+    }
+  }
 }
 
 
-// Math.floor => arredonda para baixo.
-//Math.random => sorteia palavra
